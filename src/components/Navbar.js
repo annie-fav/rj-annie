db4550fc0f3b3c006bae0e29b108f1dd22f4251e
annie <fav.hala@gmail.com>
2022-05-18 10:01:16 -0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+
+// Navbar 
+
+const Navbar = () => {
+
+    return (<header>
+
+        <h1>CoderHouse</h1>
+    
+        <nav>
+          <a className="navbar" href="">Home</a>
+          <a className="navbar" href="">Shop</a>
+          <a className="navbar" href="">Contact</a>
+
+          <div className="button">
+            <input type="submit" id="button-background-color" value="Background-Color"/>
+          </div>
+          
+          <div className="button">
+            <input type="submit" id="button-usuario" value="Usuario"/>
+          </div>
+          <br/>
+        </nav>
+ 
+      </header>
+
+               )
+}
+
+export default Navbar
+
+// ===================================================================================
+
